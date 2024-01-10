071ce351b0d6e87da21d7e74900a70b5eee45af4
trini-tech <trinimarco02@gmail.com>
2024-01-10 12:53:09 +0100

    Languages_button

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -10,21 +10,27 @@ function Languages() {
   return (
     <div className="flex justify-end">
       <div className="grid gap-4 grid-cols-3">
-        <div className="avatar justify-center">
-          <div className="w-10 rounded-full">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1280px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" onClick={() => changeLanguage('en')} />
+        <button onClick={() => changeLanguage('en')}>
+          <div className="avatar justify-center">
+            <div className="w-10 rounded-full">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Flag_of_the_United_Kingdom_%281-2%29.svg/1280px-Flag_of_the_United_Kingdom_%281-2%29.svg.png" />
+            </div>
           </div>
-        </div>
-        <div className="avatar justify-center">
-          <div className="w-10 rounded-full">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Spain_%28Civil%29.svg/750px-Flag_of_Spain_%28Civil%29.svg.png?20110426012613" onClick={() => changeLanguage('es')} />
+        </button>
+        <button onClick={() => changeLanguage('es')}>
+          <div className="avatar justify-center">
+            <div className="w-10 rounded-full">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Flag_of_Spain_%28Civil%29.svg/750px-Flag_of_Spain_%28Civil%29.svg.png?20110426012613" />
+            </div>
           </div>
-        </div>
-        <div className="avatar justify-center">
-          <div className="w-10 rounded-full">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Catalonia.svg" onClick={() => changeLanguage('cat')} />
+        </button>
+        <button onClick={() => changeLanguage('cat')}>
+          <div className="avatar justify-center">
+            <div className="w-10 rounded-full">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Flag_of_Catalonia.svg" />
+            </div>
           </div>
-        </div>
+        </button>
       </div>
     </div>
   );
